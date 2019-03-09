7e5707683e50f8bd54a313199be67986f33807a9
luiz-junior <junior.rodrigues0222@gmail.com>
2019-03-09 13:57:24 -0300

    first commit

---
+++ src/app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const consign = require('consign');
+const app = express();
+
+const knex = require('knex');
+const knexfile = require('../knexfile');
+
+// TODO criar chaveamento dinâmico
+app.db = knex(knexfile.test);
+
+app.use(express.json());
+
+consign({ cwd: 'src', verbose: false })
+    .include('./config/middlewares.js')
+    .then('./routes')
+    .then('./config/routes.js')
+    .into(app);
+
+app.get('/', (req, res) => res.status(200).send());
+
+module.exports = app;
