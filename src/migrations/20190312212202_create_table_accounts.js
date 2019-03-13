a01b8b8c43d39038f0d398c515b5b905c6d28bb5
luiz-junior <junior.rodrigues0222@gmail.com>
2019-03-12 22:11:43 -0300

    created migration accounts

---
+++ src/migrations/20190312212202_create_table_accounts.js
@@ -0,0 +1,15 @@
+
+exports.up = (knex, Promise) => {
+    return knex.schema.createTable('accounts', (t) => {
+        t.increments('id').primary();
+        t.string('name').notNull();
+        t.integer('user_id')
+            .references('id')
+            .inTable('users')
+            .notNull()
+    });
+};
+
+exports.down = (knex, Promise) => {
+    return knex.schema.dropTable('accounts')
+};
--- src/routes/users.js
+++ src/routes/users.js
@@ -1,5 +1,4 @@
 module.exports = (app) => {
-
     const findAll = (req, res) => {
         app.services.user.findAll()
             .then(result => res.status(200).json(result))
